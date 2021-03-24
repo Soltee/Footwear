7ba4e5504440c8e66e3f6e184a36ed4f13407f20
Soltee <soltee.13@gmail.com>
2021-03-24 15:56:42 +0545

    updated  node packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,20 +3,13 @@ const tailwindcss = require('tailwindcss');
 // require('dotenv').config(); 
 
 require('laravel-mix-purgecss');
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
 
-//  mix.browserSync('localhost:8000');
+mix.override((config) => {
+    delete config.watchOptions;
+});
+
 mix
-    .js('resources/js/app.js', 'public/js')
+    .js('resources/js/app.js', 'public/js').vue()
     .sass('resources/scss/app.scss', 'public/css')
     .options({
         processCssUrls: false,
